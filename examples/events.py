edff553bb2288730025c0f4cccb7f71b43d77597
mpromonet <michel.promonet@free.fr>
2016-02-24 22:35:06 +0100

    add pullpoint access

--- examples/events.py
+++ examples/events.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
-import os.path
 from onvif import ONVIFCamera
 __author__ = 'vahid'
 
 
-this_dir = os.path.abspath(os.path.dirname(__file__))
-wsdl_dir = os.path.abspath(os.path.join(this_dir, '../wsdl'))
-
-def get_event_srv():
-    mycam = ONVIFCamera('192.168.1.10', 8899, 'admin', 'admin', wsdl_dir) #, no_cache=True)
-    #print mycam.devicemgmt.GetServices()
-    event_service = mycam.create_events_service()
-    return event_service
-
-
 if __name__ == '__main__':
-    e_service = get_event_srv()
-    pp = e_service.GetEventProperties()
+    mycam = ONVIFCamera('192.168.1.10', 8899, 'admin', 'admin') #, no_cache=True)
+    event_service = mycam.create_events_service()
+    print(event_service.GetEventProperties())
+    
+    pullpoint = mycam.create_pullpoint_service()
+    req = pullpoint.create_type('PullMessages')
+    req.MessageLimit=100
+    print(pullpoint.PullMessages(req))
